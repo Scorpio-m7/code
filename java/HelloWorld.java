9f7007ee589108281e0e093226e8cf2e8dc53adf
king-notfound404 <51915181+king-notfound404@users.noreply.github.com>
2021-03-28 23:06:04 +0800

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {/*类名要和文件名一致*/
+public class HelloWorld {/*类名要和文件名一致,大驼峰命名类*/
 	public static void main(String[] args) {//程序起点
 		System.out.println("Hello,World!");//打印输出
 	}
