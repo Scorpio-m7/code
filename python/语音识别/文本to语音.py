b3341dc621c2b1e9a188eec143edb1764d8a98b1
king-notfound404 <51915181+king-notfound404@users.noreply.github.com>
2021-03-31 17:28:53 +0800

    Update 文本to语音.py

--- python/语音识别/文本to语音.py
+++ python/语音识别/文本to语音.py
@@ -9,3 +9,14 @@ str=open("1.txt", encoding="utf-8").read()#打开文本
 sperker.Speak(str)#朗读
 sperker.Speak('我是赵桐')
 del sperker#释放对象
+#***********************************
+from comtypes.client import CreateObject
+from comtypes.gen import SpeechLib
+engine=CreateObject("SAPI.SpVoice")
+stream=CreateObject('SAPI.SpFileStream')#输出流
+stream.open('1.wav',SpeechLib.SSFMCreateForWrite)
+engine.AudioOutputStream=stream#接通管道
+f=open('1.txt','r',encoding='utf-8')#读取文本内容
+engine.speak(f.read())#输出到1.wav
+f.close()#关闭文件
+stream.close()#输出流关闭
