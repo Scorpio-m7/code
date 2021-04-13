7caad5f9669de1730e4d31109a1460b237d8b11d
king-notfound404 <51915181+king-notfound404@users.noreply.github.com>
2021-04-13 17:15:12 +0800

    Update Demo01Const.java

--- java/Demo01Const.java
+++ java/Demo01Const.java
@@ -9,10 +9,9 @@
  */
 public class Demo01Const{
 	public static void main(String[] args) {
-		System.out.println("");//字符串常量
+		System.out.println("字");/*字符串常量,可以为空*/
 		System.out.println(-3.14);//整数&浮点数常量
-		System.out.println('a');//字符常量有且仅有一个字符
-		System.out.println(ture);//布尔常量
-		
+		System.out.println('a');//字符常量有且仅有一个字符，不可为空
+		System.out.println(true);//布尔常量
 	}
 }
