6e85eeb566568c3981967f835c9632ae0a3ed21f
king-notfound404 <51915181+king-notfound404@users.noreply.github.com>
2021-04-25 13:45:26 +0800

    Delete 图像处理.py

--- python/图像处理.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image#导入Image模块
-img=Image.open('img1.jpg')#打开图片
-img.show()#显示图片
-print('图片的格式',img.format,'\n大小',img.size,'\n宽度',img.width,'高度',img.height,'\n获取(100,100)处RGB值',img.getpixel((100,100)))#查看图片信息
